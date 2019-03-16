36674a3914240b3436e6d3cd7c3e6d3ee8d3695f
SmartChen2 <40797509+SmartChen2@users.noreply.github.com>
2019-03-16 10:13:12 +0800

    sort 2d array, sort object array, cmp

--- cmp.cpp
+++ cmp.cpp
@@ -3,41 +3,30 @@
 #include <algorithm>
 
 using namespace std;
+#include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 struct Node {
 	int a, b;
 };
-
 bool cmp (Node x, Node y) {
 	if (x.a <= y.a)
 		return true;
 	return false;
 }
-
 int main() {
 	int N;
 	scanf("%d", &N);
 	Node no[1000];
 	int cnt = 0;
-	for (int i = 1; i * i < N; i++) {
-		for(int j = 1; j * j < N; j++) {
-			if (i * i + j * j == N) {
-				if (i <= j) {
-					no[cnt].a = i;
-					no[cnt++].b = j;
-				}			
-			}
-		}
+	 for (int i = 0; i < N; i++) {
+		no[i].a = rand();
+		no[i].b = rand();
 	}
-	sort(no, no + cnt, cmp);
-	if (cnt == 0) {
-		printf("No Solution\n");
-	}
-	else
-	for (int i = 0; i < cnt; i++) {
+	sort(no, no + N, cmp);
+	
+	for (int i = 0; i < N; i++) {
 		printf("%d %d\n", no[i].a, no[i].b);
 	}
 	return 0;
-	
 }
